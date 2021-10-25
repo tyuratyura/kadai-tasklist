e8d3e4c8bf7fe4c3d26306e1309dbec30fe92f53
yuko.iba <exast.06.ong@outlook.jp>
2021-10-25 18:37:34 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
   root to: 'tasks#index'
 
   resources :tasks
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :create]
 end
+
