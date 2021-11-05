df959c89d6a3e09eb9223081de3f32ed181794e4
yuko.iba <exast.06.ong@outlook.jp>
2021-11-05 13:04:03 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,6 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:edit, :update, :destroy]
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:show, :edit]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
   def index
    @tasks = current_user.tasks
@@ -51,9 +50,6 @@ end
 
   private
 
-def set_task
-  @task=Task.find(params[:id])
-end
 
 def task_params
     params.require(:task).permit(:content,:status)
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
-  resources :users, only: [:index,   :create]
+  resources :users, only: [:create]
 
 
   resources :tasks
