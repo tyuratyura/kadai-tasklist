15b2d1df6c98b833a30e5d466af4d002f24a6c83
yuko.iba <exast.06.ong@outlook.jp>
2021-11-05 11:20:14 +0000

    task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:edit, :update, :destroy]
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:show]
+  before_action :correct_user, only: [:show, :edit]
 
   def index
    @tasks = current_user.tasks
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index]
   
-  
-  def index
-    @pagy, @users = pagy(User.order(id: :desc), items: 25)
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
-
   def new
     @user = User.new
   end
